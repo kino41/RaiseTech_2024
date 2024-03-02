43e9f557c56f9875aff2c32c04c1d2712fa31aca
Natsumi <nat.k@kino41.local>
2024-03-02 23:22:36 +0900

    パッケージ名の変更

--- src/main/java/com/RaiseTech8/MyBatisdemo/NameController.java
+++ src/main/java/com/RaiseTech8/MyBatisdemo/NameController.java
@@ -1,4 +1,4 @@
-package com.RaiseTech8.MyBatisdemo;
+package com.raisetech8.mybatisdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
